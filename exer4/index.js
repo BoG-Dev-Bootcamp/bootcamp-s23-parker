39e33173d48d163e05e3a180364fc18f1ab6b8a8
Parker Abramson <parkerrabramson@gmail.com>
2023-02-02 19:24:33 -0500

    Add figlet to pokemon name output

--- exer4/index.js
+++ exer4/index.js
@@ -1,5 +1,18 @@
 import axios from "axios";
+import figlet from "figlet";
 
 const response = axios.get("https://pokeapi.co/api/v2/pokemon/" + Math.floor((Math.random() * (1008 - 1 + 1) + 1)))
-    .then((response) => console.log(response.data["name"]))
+    .then((response) => {
+        figlet.text(response.data["name"], {
+            font: "Ghost",
+        }, function (error, data) {
+            if (error) {
+                console.log("There is an issue:")
+                console.dir(error)
+                return
+            }
+
+            console.log(data)
+        })
+    })
     .catch((error) => console.log(error))
